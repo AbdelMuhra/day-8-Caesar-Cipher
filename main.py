c1b6d34de49e1341960153bdb9d051690949006b
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-01 18:58:32 +0000

    date

--- main.py
+++ main.py
@@ -2,5 +2,13 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
+from datetime import date
 
+name=input("Name? \n")
+def welcome():
+  print(f"hello {name} how can i help you today")
 
+welcome()
+ask_date = input("would you like to know the date? Y|N \n")
+if ask_date == "Y":
+  print(date.today())
