7f2dd455898857ea9979d36df19beb561f13fe1a
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-11 03:35:57 +0000

    fixed an out of index bug.

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ shift = int(input("Type the shift number:\n"))
 def encrypt(plain_text, shift_amount):
   cipher_text = ""
   for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
+    if alphabet.index(letter) >= 20:
+      position = alphabet.index(letter)
+      new_position = position - shift_amount
+      new_letter = alphabet[new_position]
+      cipher_text += new_letter
+    else:
+      position = alphabet.index(letter)
+      new_position = position + shift_amount
+      new_letter = alphabet[new_position]
+      cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
 
 encrypt(plain_text=text, shift_amount=shift)
