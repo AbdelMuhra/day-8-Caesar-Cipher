927da890f14aa425740b6d036f15c725254a3e29
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-11 03:28:10 +0000

    removed comments

--- main.py
+++ main.py
@@ -4,14 +4,8 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
 def encrypt(plain_text, shift_amount):
-  #TODO-2: Inside the encrypt function, shift each letter of the text forwards in the alphabet by the shift amount and print the encrypted text.  
-  #e.g. 
-  #plain_text = "hello"
-  #shift = 5
-  #cipher_text = "mjqqt"
-  #print output: "The encoded text is mjqqt"
   cipher_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
@@ -20,5 +14,4 @@ def encrypt(plain_text, shift_amount):
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 encrypt(plain_text=text, shift_amount=shift)
