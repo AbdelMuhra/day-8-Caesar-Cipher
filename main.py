06667f70eb1913f51768058547e5f5372374d829
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-13 03:51:01 +0000

    removed the encode() and decode() function and creqated a caesar functon that contains both.

--- main.py
+++ main.py
@@ -4,38 +4,36 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def caesar(plain_text, shift_amount, direction_type):
+  if direction_type == "encode":
+    cipher_text = ""
+    for letter in plain_text:
+      if alphabet.index(letter) >= 20:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+      else:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+  elif direction_type == "decode":
+    decoded_text = ""
+    for letter in plain_text:
+      if alphabet.index(letter) >= 20:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        decoded_text += new_letter
+      else:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        decoded_text += new_letter
+    print(f"The decoded text is {decoded_text}")
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    if alphabet.index(letter) >= 20:
-      position = alphabet.index(letter)
-      new_position = position - shift_amount
-      new_letter = alphabet[new_position]
-      cipher_text += new_letter
-    else:
-      position = alphabet.index(letter)
-      new_position = position + shift_amount
-      new_letter = alphabet[new_position]
-      cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
 
-def decrypt(plain_text, shift_amount):
-  decoded_text = ""
-  for letter in plain_text:
-    if alphabet.index(letter) >= 20:
-      position = alphabet.index(letter)
-      new_position = position - shift_amount
-      new_letter = alphabet[new_position]
-      decoded_text += new_letter
-    else:
-      position = alphabet.index(letter)
-      new_position = position - shift_amount
-      new_letter = alphabet[new_position]
-      decoded_text += new_letter
-  print(f"The decoded text is {decoded_text}")
-
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(plain_text=text,shift_amount=shift)
+caesar(plain_text=text, shift_amount=shift,direction_type=direction)
+  
