aa5ee1a6ad44c9289685e6da9dc48d633b4773fe
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-14 08:44:37 +0000

    added a loop to keep the program going until user specifies to end it.

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 from art import logo
 print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-shift = shift % 26
 def caesar(start_text, shift_amount, cipher_direction):
   end_text=""
   if cipher_direction == "decode":
@@ -21,5 +17,17 @@ def caesar(start_text, shift_amount, cipher_direction):
       end_text+=char
   print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(start_text=text, shift_amount=shift,cipher_direction=direction)
-  
+continue_program = True
+while continue_program:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift,cipher_direction=direction)
+
+  result = input("Type 'yes' to continue. Otherwise type 'no' to end program.\n")
+  if result =='no':
+    continue_program = False
+    print("Good bye")
+
